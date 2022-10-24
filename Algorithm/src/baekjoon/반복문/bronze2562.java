a0836dd6879b0c97700b1a4ff56ee3ce5f70b368
재희 <@LAPTOP-NCHE4A2T>
2022-10-24 13:52:03 +0900

    gd

---
+++ Algorithm/src/baekjoon/반복문/bronze2562.java
@@ -0,0 +1,14 @@
+package baekjoon.¹Ýº¹¹®;
+
+import java.util.Scanner;
+
+public class bronze2562 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+	}
+
+}
