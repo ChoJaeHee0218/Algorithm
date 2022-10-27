9a59851135f6b0fb982095e442ad9e603b61b0b9
CJaehee <dmsrlwl1@naver.com>
2022-10-27 18:31:01 +0900

    Merge branch 'master' of https://github.com/ChoJaeHee0218/Algorithm.git

---
+++ Algorithm/src/baekjoon/문자열/bronze11654.java
@@ -0,0 +1,15 @@
+package baekjoon.¹®ÀÚ¿­;
+
+import java.util.Scanner;
+
+public class bronze11654 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int str = sc.next().charAt(0);
+		
+		System.out.println(str);
+	}
+
+}
---
+++ Algorithm/src/baekjoon/문자열/bronze11720.java
@@ -0,0 +1,20 @@
+package baekjoon.¹®ÀÚ¿­;
+
+import java.util.Scanner;
+
+public class bronze11720 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		String str = sc.next();
+		int sum = 0;
+		
+		for(int i = 0; i < str.length(); i++) {
+			sum += str.charAt(i) -'0';
+		}
+		System.out.println(sum);
+	}
+
+}
