ccddef441483834d6c1d7cda76c974f701b57f98
ChoJaeHee0218 <59820993+ChoJaeHee0218@users.noreply.github.com>
2022-10-27 18:58:21 +0900

    [Bronze III] Title: 오븐 시계, Time: 236 ms, Memory: 18524 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 Scanner in = new Scanner(System.in);
+		 
+	        int a = in.nextInt();
+	        int b = in.nextInt();
+	 
+	        int c = in.nextInt();
+	        
+	        int min = 60 * a + b;
+	        min += c;
+	 
+	        int hour = (min / 60) % 24;
+	        int minute = min % 60;
+	 
+	        System.out.println(hour + " " + minute);
+	}
+
+}
