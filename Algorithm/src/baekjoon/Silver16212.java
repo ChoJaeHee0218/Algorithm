29a6830048678fcba4b5c2cade157f0f92a85c02
CJaehee <dmsrlwl1@naver.com>
2022-10-31 19:47:15 +0900

    gd

---
+++ Algorithm/src/baekjoon/Silver16212.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Silver16212 {
+
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int[] array = new int[N];
+
+        for (int i = 0 ; i < N ; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(array);
+
+        for (int i = 0 ; i < N ; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+	}
+
+}
---
+++ Algorithm/src/baekjoon/문자열/bronze9093.java
@@ -0,0 +1,18 @@
+package baekjoon.¹®ÀÚ¿­;
+
+import java.util.Scanner;
+
+public class bronze9093 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		String str = sc.next();
+		
+		for(int i = 0; i < str.length(); i++) {
+			
+		}
+	}
+
+}
