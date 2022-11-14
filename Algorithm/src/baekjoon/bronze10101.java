6489eef1a0e20e1e7edccc521c2dc530cbc2d14a
재희 <@LAPTOP-NCHE4A2T>
2022-11-14 16:45:52 +0900

    gc

---
+++ Algorithm/src/baekjoon/bronze10101.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class bronze10101 {
+
+	public static void main(String[] args)throws Throwable {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        if (a + b + c != 180) {
+            System.out.print("Error");
+        } else {
+            if (a == 60 && b == 60 && c == 60) {
+                System.out.print("Equilateral");
+            } else if (a == b || b == c || c == a) {
+                System.out.print("Isosceles");
+            } else {
+                System.out.print("Scalene");
+            }
+        }
+    }
+}
+
--- Algorithm/src/baekjoon/bronze10820.java
+++ Algorithm/src/baekjoon/bronze10820.java
@@ -6,13 +6,31 @@ public class bronze10820 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		String N = sc.nextLine();
-		
-		
+	
+		while(sc.hasNextLine()) {
+			String N = sc.nextLine();
+			int cnt = 0;
+			int cnt2 = 0;
+			int cnt3 = 0;
+			int cnt4 = 0;
+			for(int i=0;i<N.length();i++) {
+				if(N.charAt(i) >= 'a' && N.charAt(i) <= 'z') {
+					cnt2++;
+				}
+				else if(N.charAt(i) >= 'A' && N.charAt(i) <= 'Z') {
+					cnt++;
+				}
+				else if(N.charAt(i) >= '0' && N.charAt(i) <= '9') {
+					cnt3++;
+				}
+				else if(N.charAt(i) == ' ') {
+					cnt4++;
+				}
+			}
+			System.out.println(cnt2+" "+cnt+" "+  cnt3 +" "+ cnt4);
 			
-		
-		
-	}
+		}
+			
+		}
 
 }
