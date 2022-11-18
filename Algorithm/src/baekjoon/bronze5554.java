99a80b140e797e1753888b8523f0189ab9aa2c35
재희 <@LAPTOP-NCHE4A2T>
2022-11-18 16:23:02 +0900

    fdg

--- Algorithm/src/baekjoon/bronze5554.java
+++ Algorithm/src/baekjoon/bronze5554.java
@@ -1,10 +1,22 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class bronze5554 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int result = 0;
+		
+		for(int i=0; i<4; i++) {
+			result += sc.nextInt();
+		}
+		int min = result / 60;
+		int max = result % 60;
+		
+		System.out.println(min);
+		System.out.println(max);
 	}
 
 }
